414204f8947b5f568d2e853fad0fb55effe23bb2
Ivan Svidinskiy <icommerce777@gmail.com>
2025-11-15 16:33:27 +0300

    feat(lesson1): home work

---
+++ src/modules/cart.js
@@ -0,0 +1,20 @@
+const cart = () => {
+    /** @type {HTMLDivElement} */
+    const cartBtn = document.getElementById('cart');
+    /** @type {HTMLDivElement} */
+    const cartModal = document.querySelector('.cart');
+    /** @type {HTMLDivElement} */
+    const cartClose = document.querySelector('.cart-close');
+
+    const openModal = () => {
+        cartModal.style.display = 'flex';
+    }
+    const closeModal = () => {
+        cartModal.style.display = 'none';
+    }
+
+    cartBtn.addEventListener('click', openModal);
+    cartClose.addEventListener('click', closeModal);
+}
+
+export default cart
---
+++ src/modules/second.js
@@ -0,0 +1,14 @@
+const second = () => {
+    /** @type {HTMLDivElement} */
+    const cartBtn = document.getElementById('cart');
+    /** @type {HTMLDivElement} */
+    const cartModal = document.querySelector('.cart');
+    /** @type {HTMLDivElement} */
+    const cartClose = document.querySelector('.cart-close');
+
+    console.log(cartBtn);
+    console.log(cartModal);
+    console.log(cartClose);
+}
+
+export default second
